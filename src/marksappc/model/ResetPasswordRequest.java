77a783b5ccf40d1894b114f59f4179450bd665ec
chelsangya <csangya@gmail.com>
2025-05-30 11:24:54 +0545

    reset ongoing

--- src/marksappc/model/ResetPasswordRequest.java
+++ src/marksappc/model/ResetPasswordRequest.java
@@ -9,5 +9,23 @@ package marksappc.model;
  * @author sangyakoirala
  */
 public class ResetPasswordRequest {
-    
+      private String email;
+    private String password;
+//    public methods
+    public ResetPasswordRequest(String email, String password){
+        this.email=email;
+        this.password = password;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword (){
+        return password;
+    }
 }
